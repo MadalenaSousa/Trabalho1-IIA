8f9da8758ad4423353688da1ca8a7fbb5dd8ab4b
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-02-25 20:09:01 +0000

    n entendo nada desta merda

--- Assets/Scripts/LinearRobotUnitBehaviour.cs
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -7,17 +7,24 @@ public class LinearRobotUnitBehaviour : RobotUnit
     public float weightResource;
     public float resourceValue;
     public float resouceAngle;
+    public float blockValue;
+    public float blockAngle;
 
     void Update()
     {
 
         // get sensor data
         resouceAngle = resourcesDetector.GetAngleToClosestResource();
-
         resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+
+        blockAngle = blockDetector.GetAngleToClosestObstacle();
+        blockValue = blockDetector.GetLinearOuput();
+
+        print(blockAngle);
 
         // apply to the ball
         applyForce(resouceAngle, resourceValue); // go towards
+        //applyForce(blockAngle, blockValue);
 
         
 
