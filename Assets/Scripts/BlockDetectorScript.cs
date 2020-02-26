ce28311443a9467c894cd49da37186b9d655b292
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-02-26 16:46:48 +0000

    exercicio

--- Assets/Scripts/BlockDetectorScript.cs
+++ Assets/Scripts/BlockDetectorScript.cs
@@ -26,6 +26,19 @@ public class BlockDetectorScript : MonoBehaviour
     void FixedUpdate()
     {
         // YOUR CODE HERE
+        ObjectInfo wall;
+        wall = GetClosestWall();
+        if (wall != null)
+        {
+            angleToClosestObj = wall.angle;
+            strength = 1.0f / (wall.distance + 1.0f);
+        }
+        else
+        { // no object detected
+            strength = 0;
+            angleToClosestObj = 0;
+        }
+        //FIM DO CÓDIGO NOVO
 
     }
 
@@ -42,12 +55,58 @@ public class BlockDetectorScript : MonoBehaviour
     public virtual float GetGaussianOutput()
     {
         // YOUR CODE HERE
+
         throw new NotImplementedException();
     }
 
     public virtual float GetLogaritmicOutput()
     {
         // YOUR CODE HERE
+
         throw new NotImplementedException();
     }
+
+    // YOUR CODE HERE
+    public List<ObjectInfo> GetVisibleObjects(string objectTag)
+    {
+        RaycastHit hit;
+        List<ObjectInfo> objectsInformation = new List<ObjectInfo>();
+
+        for (int i = 0; i * angleOfSensors <= 360f; i++)
+        {
+            if (Physics.Raycast(this.transform.position, Quaternion.AngleAxis(-angleOfSensors * i, initialTransformUp) * initialTransformFwd, out hit, rangeOfSensors))
+            {
+
+                if (hit.transform.gameObject.CompareTag(objectTag))
+                {
+                    if (debugMode)
+                    {
+                        Debug.DrawRay(this.transform.position, Quaternion.AngleAxis((-angleOfSensors * i), initialTransformUp) * initialTransformFwd * hit.distance, Color.red);
+                    }
+                    ObjectInfo info = new ObjectInfo(hit.distance, angleOfSensors * i + 90);
+                    objectsInformation.Add(info);
+                }
+            }
+        }
+
+        objectsInformation.Sort();
+
+        return objectsInformation;
+    }
+
+    public ObjectInfo[] GetVisibleWalls()
+    {
+        return (ObjectInfo[])GetVisibleObjects("Wall").ToArray();
+    }
+
+    public ObjectInfo GetClosestWall()
+    {
+        ObjectInfo[] a = (ObjectInfo[])GetVisibleObjects("Wall").ToArray();
+        if (a.Length == 0)
+        {
+            return null;
+        }
+        return a[a.Length - 1];
+    }
+    //FIM DO CÓDIGO NOVO
 }
--- Assets/Scripts/LinearRobotUnitBehaviour.cs
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -8,18 +8,23 @@ public class LinearRobotUnitBehaviour : RobotUnit
     public float resourceValue;
     public float resouceAngle;
 
+    public float blockValue;
+    public float blockAngle;
+
     void Update()
     {
 
         // get sensor data
         resouceAngle = resourcesDetector.GetAngleToClosestResource();
-
         resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+
+        blockAngle = blockDetector.GetAngleToClosestObstacle();
+        blockValue = weightResource * blockDetector.GetLinearOuput();
 
         // apply to the ball
         applyForce(resouceAngle, resourceValue); // go towards
+        applyForce(blockAngle, -blockValue);
 
-        
 
     }
 
