196a52e49d939f59f34e287a00d4b34656dc0153
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-03-14 20:00:22 +0000

    ...

--- Assets/Scripts/GaussianRobotUnitBehaviour.cs
+++ Assets/Scripts/GaussianRobotUnitBehaviour.cs
@@ -7,6 +7,9 @@ public class GaussianRobotUnitBehaviour : RobotUnit
     public float weightResource;
     public float resourceValue;
     public float resourceAngle;
+    public float weightBlock;
+    public float blockValue;
+    public float blockAngle;
     public float largura, centro;
     public float resource_minX, resource_maxX, resource_minY, resource_maxY;
     public float block_minX, block_maxX, block_minY, block_maxY;
@@ -18,6 +21,9 @@ public class GaussianRobotUnitBehaviour : RobotUnit
         // get sensor data
         resourceAngle = resourcesDetector.GetAngleToClosestResource();
         resourceValue = weightResource * resourcesDetector.GetGaussianOutput(centro, largura, resource_minX, resource_maxX, resource_minY, resource_maxY);
+
+        blockAngle = blockDetector.GetAngleToClosestObstacle();
+        blockValue = weightBlock * blockDetector.GetGaussianOutput(centro, largura, block_minX, block_maxX, block_minY, block_maxY);
 
         // apply to the ball
         applyForce(resourceAngle, resourceValue); // go towards, slow first, then fast, then slow again
